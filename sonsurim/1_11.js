e1c0861bf400282a0aad54044616bb11b8f6dce9
sonsurim <surim014@naver.com>
2021-07-29 02:25:21 +0900

    [손수림] 1_11 대문자 찾기

---
+++ sonsurim/1_11.js
@@ -0,0 +1,22 @@
+// Case1. split + for
+function solution(s) {
+  let answer = 0;
+  let arr = s.split('');
+
+  for (let i = 0; i < arr.length; i++ ) {
+    if (arr[i] === arr[i].toUpperCase()) answer++
+  }
+
+  return answer;
+}
+
+let str="KoreaTimeGood";
+console.log(solution(str));
+
+// Case2. Regexp
+function solution2(s) {
+  return s.match(/[A-Z]/g).length;
+}
+
+let str2="KoreaTimeGood";
+console.log(solution2(str2));
