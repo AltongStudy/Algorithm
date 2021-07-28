4f8aa97cf3154cd2fbebb0a966720a359957d9ad
JY <goawmfhfl1@naver.com>
2021-07-28 10:39:31 +0900

    [0728]1_9

--- goawmfhfl/1_8.js
+++ goawmfhfl/1_8.js
@@ -8,7 +8,7 @@ function solution(arr){
 
     for(let i=0; i<arr.length; i++){
         for(let j=i+1; j<arr.length; j++){
-            if(sum - (arr[i]+arr[j]) === 100){
+            if(sum - (arr[i]+arr[j) === 100){
                 arr.splice(j,1)
                 arr.splice(i,1)
             }
