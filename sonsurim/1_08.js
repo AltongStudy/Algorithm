c8015a7cd6672dfefdef8a46c5e5051dc20da0bc
sonsurim <surim014@naver.com>
2021-07-31 03:05:27 +0900

    [손수림] 1-8 일곱 난쟁이 (코드리뷰 피드백 반영)

--- sonsurim/1_08.js
+++ sonsurim/1_08.js
@@ -1,3 +1,4 @@
+// Case1.
 function solution(arr) {
   let answer = arr;
   let sum = arr.reduce((arr, cur) => arr + cur, 0);
@@ -5,7 +6,8 @@ function solution(arr) {
   for(let i =0; i< arr.length; i++) {
     for (let j = i+1; j < arr.length; j++) {
       if ( sum - (arr[i] + arr[j]) === 100) {
-        answer.splice(answer.indexOf(arr[i]), 2);
+        answer.splice(j, 1);
+        answer.splice(i, 1);
       }
     }
   }
@@ -13,5 +15,14 @@ function solution(arr) {
   return answer;
 }
 
-let arr=[20, 7, 23, 19, 10, 15, 25, 8, 13];
+let arr=[20, 7, 23, 15, 19, 10, 25, 8, 13];
 console.log(solution(arr));
+
+// Case 2.
+function solution2(arr) {
+  let sum = arr.reduce((arr, cur) => arr + cur, 0);
+  return arr.filter( item => !(arr.indexOf(sum - 100 - item) > 0));
+}
+
+let arr2=[20, 7, 2, 23, 19, 10, 38, 8, 13];
+console.log(solution2(arr2));
