805fa6f1fc5481fbd6d5942b96499156df5c054a
JY <goawmfhfl1@naver.com>
2021-08-17 20:30:31 +0900

    2-6. 격자판 최대합

--- goawmfhfl/2.6.js
+++ goawmfhfl/2.6.js
@@ -3,7 +3,6 @@
     function solution(arr){
         let answer = Number.MIN_SAFE_INTEGER;
         let n = arr.length;
-
         let rowSum = 0; // 행
         let columnSum = 0; // 열
         let slash = 0 // 대각선
@@ -17,12 +16,22 @@
                 columnSum += arr[j][i] // 열
             }
             answer = Math.max(answer,columnSum,rowSum) // 행 열의 최대합
+            console.log(`
+            answer: ${answer}
+            columnSum: ${columnSum}
+            rowSum: ${rowSum}
+            `);
         }
         for(let i=0; i<n; i++){
             slash += arr[i][i] // 대각선
             backSlash += arr[i][n-i-1] //역 대각선
         }
         answer = Math.max(answer, slash, backSlash)
+        console.log(`
+        answer: ${answer}
+        slash: ${slash}
+        backSlash: ${backSlash}
+        `);
 
 
         return answer;
@@ -40,23 +49,37 @@
 // 🦾 다른 방식 문제 풀이 🦾
 
 {    
-function solution(arr) {
-let row = new Array(arr.length).fill(0); 
-let column = new Array(arr.length).fill(0); 
-let cross = 0;
-let reverseCross = 0;
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-        if (i === j) {
-        cross += arr[i][j];
-        reverseCross += arr[i][arr.length - i - 1];
+    function solution(arr) {
+        let row = new Array(arr.length).fill(0); 
+        console.log(`row: ${row}`);
+        let column = new Array(arr.length).fill(0); 
+        console.log(`column: ${column}`);
+        let cross = 0;
+        let reverseCross = 0;
+        
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < arr.length; j++) {
+                if (i === j) {
+                cross += arr[i][j];
+                reverseCross += arr[i][arr.length - i - 1];
+                }
+                row[i] += arr[i][j];
+                column[i] += arr[j][i];
+                }
+            }
+        console.log(`row: ${[...row]}`);
+        console.log(`column: ${[...column]}`);
+        console.log(`cross: ${cross}`);
+        console.log(`reverseCross: ${reverseCross}`);
+            return Math.max(...row, ...column, cross, reverseCross);
         }
-        row[i] += arr[i][j];
-        column[i] += arr[j][i];
-        }
-    }
-    return Math.max(...row, ...column, cross, reverseCross);
-}
+        let board =[
+                [10, 13, 10, 12, 15],
+                [12, 39, 30, 23, 11],
+                [11, 25, 50, 53, 15],
+                [19, 27, 29, 37, 27],
+                [19, 13, 30, 13, 19],
+            ]
+            console.log(solution(board));
     
 }
