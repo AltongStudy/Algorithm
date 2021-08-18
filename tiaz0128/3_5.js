f6dea403116782b3db54c0522f02f8f47b2ead0b
tiaz0128 <tiaz0128.dev@gmail.com>
2021-08-18 19:54:01 +0900

    3-5 수정

--- tiaz0128/3_5.js
+++ tiaz0128/3_5.js
@@ -1,16 +1,25 @@
 function solution(str){
   const strArr = str.split('')
   let result = ''
-  let idx = 1;
+  let cnt = 1;
 
-  for(let i = 0; i < strArr.length - 1; i++){
-      if(strArr[i] !== strArr[i+1]){
-          result += strArr[i] + (idx === 1 ? '' : idx)
-          idx = 1
+  strArr.forEach((item ,idx, arr) => {
+      if(item !== arr[idx+1]){
+          result += item + (cnt === 1 ? '' : cnt)
+          cnt = 1
       } else {
-          idx++
+          cnt++
       }
-  }
+  })
+
+//   for(let i = 0; i < strArr.length; i++){
+//       if(strArr[i] !== strArr[i+1]){
+//           result += strArr[i] + (idx === 1 ? '' : idx)
+//           idx = 1
+//       } else {
+//           idx++
+//       }
+//   }
 
   return result
 }
