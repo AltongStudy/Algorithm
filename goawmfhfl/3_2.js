cf491ad0d82bb2972a9b6d8bc711f441808e620b
Jae young Choi <79143800+goawmfhfl@users.noreply.github.com>
2021-08-19 19:25:02 +0900

    3-2. 유효한팰린드롬

--- goawmfhfl/3_2.js
+++ goawmfhfl/3_2.js
@@ -2,22 +2,6 @@
 
 function solution(s){
     let answer = "YES";
-    let len = s.length;
-
-    for(let i=0; i<Math.floor(len/2); i++){
-        if(s[i] !== s[len-i-1].split('').reverse().join('')) answer = "NO"
-    }
-    return answer;
-}
-
-let str =['found7',"time","study","Yduts",'emit',"7Dnuof"]
-console.log(solution(str));
-
-
-// 👨🏻‍🏫 강사님 문제 풀이 2 👨🏻‍🏫
-
-function solution(s){
-    let answer = "YES";
     s=s.toLowerCase().replace(/[^a-z]/g,'')
     if(s.split('').reverse().join('') !== s) return "NO"
     return answer;
