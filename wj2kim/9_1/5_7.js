5ecb51d627f294f99c1196ed5f1ed11fc5fa613d
wj2kim <wj2kim@gmail.com>
2021-09-02 17:58:46 +0900

    아나그램 완료

--- wj2kim/9_1/5_7.js
+++ wj2kim/9_1/5_7.js
@@ -8,22 +8,40 @@
  *
  * 앞문제와 마찬가지로 포문을 돌면서 기호를 키값으로 object 에 추가를 한다.
  * 키 값이 있으면 해당 키값으 value 를 ++ 연산 처리한다.
- * 두개의 object 생성이 완료되면 두개의 object key와 value 값이 모두 일치하는지 확인하여 yes or no 를 리턴한다.
+ * 한번 돌떄 두개의 object를 생성한다.
+ * 두개의 object 생성이 완료되면 두개의 object key와 value 값이 일치하는지 Object.entries를 통해 확인한다.
+ * 하나라도 다를시 return NO 를 해준다.
  *
  */
 
 function solution(arr1, arr2) {
-  let answer = {};
+  let answer1 = {},
+    answer2 = {};
 
-  for (let i = 0; i < [...arr].length; i++) {
-    if (!answer[arr[i]]) {
-      answer[arr[i]] = 1;
+  if (arr1.length !== arr2.length) {
+    return "NO";
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!answer1[arr1[i]]) {
+      answer1[arr1[i]] = 1;
     } else {
-      answer[arr[i]] += 1;
+      answer1[arr1[i]] += 1;
+    }
+    if (!answer2[arr2[i]]) {
+      answer2[arr2[i]] = 1;
+    } else {
+      answer2[arr2[i]] += 1;
     }
   }
 
-  return Object.keys(answer).sort((a, b) => answer[b] - answer[a])[0];
+  for (const [key, value] of Object.entries(answer1)) {
+    if (answer2[key] !== value) {
+      return "NO";
+    }
+  }
+
+  return "YES";
 }
 
 let arr1 = "abaCC";
