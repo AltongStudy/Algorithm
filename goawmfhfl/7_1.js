35316382893c36dea72be74569308a16a8a65c3d
JY <goawmfhfl1@naver.com>
2021-09-15 19:19:11 +0900

    7-1 선택정렬

--- goawmfhfl/7_1.js
+++ goawmfhfl/7_1.js
@@ -1,15 +1,19 @@
-// ✅ 7-2 버블정렬
-// 👨🏻‍🏫 강사님 문제 풀이 👨🏻‍🏫
-function solution(arr) {
+// ✅ 7-1 선택정렬
 
+// // 👨🏻‍🏫 강사님 문제 풀이 👨🏻‍🏫
+
+function solution(arr){
     let answer = arr
+
     for(let i=0; i<arr.length-1; i++){
-        for(let j=0; j<arr.length-1-i; j++){
-            if(arr[j]>arr[j+1]) [arr[j],arr[j+1]]=[arr[j+1],arr[j]]
-        }
-    }
-    return answer
+        let idx = i
+        for(let j=i+1; j<arr.lengh; j++){
+				if(arr[idx]>arr[j]) idx = j;
+	    }
+		[arr[i], arr[idx] = [arr[idx], arr[i]];
+	}
+  return answer;
 }
 
-let arr = [13, 5, 11, 7, 23, 15]
+let arr = [13,5,11,7,23,15]
 console.log(solution(arr));
